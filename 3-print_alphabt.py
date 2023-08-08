b5724b06240536a58d65d196476a788bc73bb85a
wafae10 <zwafae13@gmail.com>
2023-08-08 03:02:36 -0700

    up

---
+++ 3-print_alphabt.py
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
+#!/usr/bin/python3
+for letter in range(97, 123):
+    if chr(letter) is not 'q' and chr(letter) is not 'e':
+        print("{}".format(chr(letter)), end="")
