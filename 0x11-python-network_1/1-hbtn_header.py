aed999698553ae9d6949245ff8edf29f970119a7
wafae10 <zwafae13@gmail.com>
2023-12-01 03:36:23 -0800

    up

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Python script that takes in a URL, sends a request to the URL and displays the value of the X-Request-Id variable found in the header of the response."""
+
+if __name__ == '__main__':
+    import sys
+    import urllib.request
+    url = sys.argv[1]
+    with urllib.request.urlopen(url) as response:
+        url_res = response.info()
+        print(url_res['X-Request-Id'])
